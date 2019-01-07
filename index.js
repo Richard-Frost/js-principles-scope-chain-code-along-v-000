ada148fd94831bc230edce9da04fbaa3c474fedd
Richard Frost <frost.rchrd@gmail.com>
2019-01-07 18:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function upperCaseCustomerName() {
   customerName = customerName.toUpperCase()
 }
 
+function setBestCustomer() {
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "william"
+}
