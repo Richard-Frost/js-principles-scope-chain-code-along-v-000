cf32287f6a38d2a499171504035755e9891796aa
Richard Frost <frost.rchrd@gmail.com>
2019-01-07 18:36:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-car customerName = 'bob'
+var customerName = 'bob'
+const leastFavoriteCustomer = "bill"
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()learn
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "william"
+}
